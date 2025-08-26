b1904dd072a5ed690dd746d561dba2e5f3d94e85
cfgrillo <caleb.grillo@gmail.com>
2025-08-25 19:39:01 -0500

    draw player

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,8 +16,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
             pygame.Surface.fill(screen, (0, 0, 0))
+            player.draw(screen)
             pygame.display.flip()
         dt = clock.tick(60)/1000
+    
 
 if __name__ == "__main__":
     main()
